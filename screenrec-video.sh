b6ee77354687c0c68ed0c78335b1e2a893a98f32
Ingolf Ankert <git@ingank.net>
2020-08-10 19:33:33 +0200

    Update screenrec-video.sh

--- screenrec-video.sh
+++ screenrec-video.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 UUID=$(< uuid.txt)
-
 ffmpeg -y -f x11grab -video_size 1920x1080 -framerate 60 -i :0.0 -vaapi_device ":0" -vf 'format=nv12,hwupload' -map 0:0 -threads 8 -aspect 16:9 -g 30 -f mp4 -bf 0 -qp 19 -quality 2 -vcodec h264_vaapi "$UUID.mp4"
